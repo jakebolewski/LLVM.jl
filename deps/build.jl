4d701fa4a42f20c7f30b81a552716394d698bd45
Jake Bolewski <jakebolewski@gmail.com>
2014-06-21 23:00:13 -0400

    rework build process, ext.jl is now generated in the when building the package fixing the LLVM version at package build time

--- deps/build.jl
+++ deps/build.jl
@@ -4,14 +4,42 @@ ENV["JULIA_ROOT"] = abspath(JULIA_HOME, "../../")
 
 cd(joinpath(Pkg.dir(), "LLVM", "deps", "src") )
 
+# Build libLLVMGeneral library
+run(`make clean`)
 run(`make`)
 
-if (!ispath("../usr")) 
-  run(`mkdir ../usr`)
+DIR = abspath(dirname(@__FILE__))
+USRDIR = abspath(joinpath(DIR, "usr"))
+USRLIBDIR = abspath(joinpath(USRDIR, "lib"))
+
+ispath(USRDIR)    || mkdir(USRDIR)
+ispath(USRLIBDIR) || mkdir(USRLIBDIR)
+
+const LIBNAME = "libllvmgeneral.$(BinDeps.shlib_ext)"
+mv(LIBNAME, joinpath(USRLIBDIR, LIBNAME))
+
+# Generate ext.jl with hardcoded paths to llvm libraries
+LLVMVER = readchomp(`llvm-config --version`)
+
+lv = VersionNumber(map(int, split(LLVMVER, ['.', '-']))...)
+if lv.major != 3 || lv.minor < 4
+    error("Only llvm versions 3.4 or 3.5 are supported")
+end 
+
+LLVM_GENRAL_DIR = Pkg.dir("LLVM", "deps", "usr", "lib")
+
+if isfile(joinpath(DIR, "ext.jl"))
+    rm(joinpath(DIR, "ext.jl"))
 end
 
-if (!ispath("../usr/lib"))
-  run(`mkdir ../usr/lib`)
-end
+EXTSRC = """
+const libllvmgeneral = find_library(["libllvmgeneral",], ["$LLVM_GENRAL_DIR"])
+@assert(libllvmgeneral != "", \"\"\"Failed to find required library libllvmgeneral. 
+                            Try re-running the package script using Pkg.build("LLVM")\"\"\")
+const libllvm = find_library(["libLLVM-$LLVMVER"])
+@assert(libllvm != "", "Failed to find llvm library libLLVM-$LLVMVER") 
+"""
 
-run(`mv libllvmgeneral.$(BinDeps.shlib_ext) ../usr/lib`)
+fh = open(joinpath(DIR, "ext.jl"), "w")
+write(fh, EXTSRC)
+close(fh)
--- deps/ext.jl
+++ deps/ext.jl
@@ -1,4 +1,5 @@
-const llvmgeneral = find_library(["libllvmgeneral",],
-                                 [Pkg.dir("LLVM", "deps", "usr", "lib"),])
-@assert(llvmgeneral != "", """Failed to find required library libllvmgeneral. 
-                              Try re-running the package script using Pkg.build(\"LLVM\")""")
+const libllvmgeneral = find_library(["libllvmgeneral",], ["/home/jake/.julia/v0.3/LLVM/deps/usr/lib"])
+@assert(libllvmgeneral != "", """Failed to find required library libllvmgeneral. 
+                            Try re-running the package script using Pkg.build("LLVM")""")
+const libllvm = find_library(["libLLVM-3.4"])
+@assert(libllvm != "", "Failed to find llvm library libLLVM-3.4") 
