d22e932a80b3457e3d8066dfff7ada850f50214b
Jake Bolewski <jakebolewski@gmail.com>
2014-07-09 00:16:44 -0400

    expand simple context impl and add tests

--- src/context.jl
+++ src/context.jl
@@ -1,23 +1,28 @@
 #------------------------------------------------------------------------------
 # Context 
 #------------------------------------------------------------------------------
-export Context, create_ctx, global_ctx, dispose_ctx, with_ctx
+export Context, create_ctx, global_ctx, dispose_ctx, with_ctx, with_global_ctx
 
 immutable ContextHandle
     ptr::Ptr{Void}
 end 
 
 Base.convert(::Type{Ptr{Void}}, handle::ContextHandle) = handle.ptr
+Base.(:(==))(h1::ContextHandle, h2::ContextHandle) = h1.ptr === h2.ptr
 
 type Context
     handle::ContextHandle
+    isglobal::Bool
+    disposed::Bool
 
-    Context(handle::ContextHandle) = begin
-        ctx = new(handle)
-        finalizer(ctx, dispose_ctx)
+    Context(handle::ContextHandle, isglobal=false) = begin
+        ctx = new(handle, isglobal, false)
+        !isglobal && finalizer(ctx, dispose_ctx)
         return ctx
     end 
 end 
+
+Base.(:(==))(ctx1::Context, ctx2::Context) = ctx1.handle === ctx2.handle
 
 # http://llvm.org/doxygen/group__LLVMCCoreContext.html#gaac4f39a2d0b9735e64ac7681ab543b4c
 function create_ctx()
@@ -26,15 +31,24 @@ end
 
 # http://llvm.org/doxygen/group__LLVMCCoreContext.html#ga0055cde9a9b2497b332d639d8844a810
 function global_ctx()
-    return Context(ccall((:LLVMGetGlobalContext, libllvm), ContextHandle, ()))
+    return Context(ccall((:LLVMGetGlobalContext, libllvm), ContextHandle, ()), true)
 end
+
+isglobalctx(c::Context) = c.isglobal
 
 # http://llvm.org/doxygen/group__LLVMCCoreContext.html#ga9cf8b0fb4a546d4cdb6f64b8055f5f57
 
-function dispose_ctx(c::ContextHandle)
+isdisposed(c::Context) = c.disposed
+
+dispose_ctx(c::ContextHandle) = begin
     ccall((:LLVMContextDispose, libllvm), Void, (ContextHandle,), c)
 end
-dispose_ctx(c::Context) = dispose_ctx(c.handle)
+dispose_ctx(c::Context) = begin
+    if !(isdisposed(c) || isglobalctx(c))
+        dispose_ctx(c.handle)
+        c.disposed = true 
+    end 
+end 
 
 with_ctx(f::Function) = begin
     ctx = create_ctx() 
@@ -44,3 +58,8 @@ with_ctx(f::Function) = begin
         dispose_ctx(ctx)
     end
 end
+
+with_global_ctx(f::Function) = begin
+    ctx = global_ctx()
+    f(ctx)
+end 
--- test/runtests.jl
+++ test/runtests.jl
@@ -63,3 +63,31 @@ facts("Data Layout") do
         @fact string(LLVM.parse_datalayout(str)) => str
     end
 end 
+
+facts("Context") do
+    context("simple constructor test") do
+        ctx = LLVM.create_ctx()
+        @fact isa(ctx, LLVM.Context) => true
+        LLVM.dispose_ctx(ctx)
+        @fact LLVM.isdisposed(ctx) => true
+    end
+
+    context("simple global context constructor test") do
+        ctx1 = LLVM.global_ctx()
+        ctx2 = LLVM.global_ctx()
+        @fact ctx1 => ctx2
+    end
+
+    context("simple with context constructor test") do
+        LLVM.with_ctx() do ctx
+            @fact isa(ctx, LLVM.Context) => true
+        end
+    end 
+
+    context("simple with global context constructor test") do
+        LLVM.with_global_ctx() do ctx
+            @fact isa(ctx, LLVM.Context) => true
+            @fact LLVM.isglobalctx(ctx) => true
+        end
+    end
+end
