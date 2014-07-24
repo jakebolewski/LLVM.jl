b6a11e1b1807e28fb5d36020b308cfe47737b242
Jake Bolewski <jakebolewski@gmail.com>
2014-07-24 09:57:59 -0400

    write equality checks for serialized / deserialized asts

--- test/constants.jl
+++ test/constants.jl
@@ -1,6 +1,28 @@
 using FactCheck 
 using LLVM
 
+check_bitcode(buf1, buf2) = buf1.data == buf2.data
+
+function check_result(ast, asm)
+    LLVM.with_global_ctx() do ctx
+        
+        mod1 = LLVM.module_from_assembly(ctx, asm)
+        mod2 = LLVM.module_from_ast(ctx, ast)
+
+        # check that we can correctly serialize both to assembly / bitcode
+        @fact LLVM.module_to_assembly(ctx, mod1) => LLVM.module_to_assembly(ctx, mod2)
+        @fact check_bitcode(LLVM.module_to_bitcode(ctx, mod1), 
+                            LLVM.module_to_bitcode(ctx, mod2)) => true
+        
+        # check that we can lift from assembly to an
+        # Ast.Module and perform equality checks on the ast
+        @fact LLVM.module_to_ast(ctx, mod1) => ast
+        
+        # check that we can produce a valid assembly string from the ast
+        @fact LLVM.module_to_assembly(ctx, mod2) => asm
+    end 
+end
+
 test_ast(typ, val, str) = begin
     ast = Ast.Module("<string>", nothing, nothing, [
             Ast.GlobalDefinition(
@@ -10,12 +32,12 @@ test_ast(typ, val, str) = begin
             Ast.GlobalDefinition(
                 Ast.GlobalVar(name=Ast.UnName(2), typ=Uint32, init=nothing)),
         ])
-    astr ="""
-; ModuleID = '<string>'
+    astr ="""; ModuleID = '<string>'
 
 @0 = $str
 @1 = external global i32
-@2 = external global i32"""
+@2 = external global i32
+"""
     return (ast, astr)
 end
 
@@ -24,12 +46,14 @@ facts("test constants") do
         ast, asm = test_ast(Uint32,
                             one(Uint32),
                             "global i32 1")
+        check_result(ast, asm)
     end
 
     context("wide integer") do
         ast, asm  = test_ast(Ast.IntType(65),
                              Ast.ConstInt(65,1),
                              "global i65 1")
+        check_result(ast, asm)
     end
 
     context("big wide integer") do
@@ -39,8 +63,10 @@ facts("test constants") do
     end
 end 
 
+#=
 ctx = LLVM.global_ctx()
 ast, asm = test_ast(Uint32, one(Uint32), "global i32 1")
 modh = LLVM.module_from_ast(ctx, ast)
 res = LLVM.module_to_ast(ctx, modh)
 #LLVM.write_assembly("/home/jake/test.llvm", modh)
+=#
